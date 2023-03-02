a7a93eddba888ec2a3e0beca1eae616765d662b7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-02 00:54:40 +0000

    remove func and put stuff in main

--- main.go
+++ main.go
@@ -2,29 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
-  "os"
-  "strconv"
 )
 
-// First arg is duration
-// Ticks for duration seconds
-func pomodoroTimer() {
-  arg := os.Args[1]
-  duration, err := strconv.Atoi(arg)
-  if err != nil {
-    os.Exit(3)
-  }
-
-  for duration > 0 {
-      fmt.Println("time left:", duration, "minute(s)")
-      time.Sleep(1 * time.Minute)
-      duration-- 
-    }
-  fmt.Println("Time up!")
-}
-
 func main() {
-  pomodoroTimer()
-}
+	arg := os.Args[1] // arg is num minutes
+	duration, err := strconv.Atoi(arg)
+	if err != nil {
+		os.Exit(3)
+	}
 
+	for duration > 0 {
+		fmt.Println("time left:", duration, "minute(s)")
+		time.Sleep(1 * time.Minute)
+		duration--
+	}
+	fmt.Println("Time up!")
+}
